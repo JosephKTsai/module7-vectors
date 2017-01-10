ecd60fbd036741b82d1d07adc210cba2e39db6a3
Joseph Tsai <josephkt@uw.edu>
2017-01-10 14:41:53 -0800

    Complete exercises 1-3

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -2,26 +2,35 @@
 
 # Create a vector `x` that has the values 10 through 20 in it (using the : operator)
 
+x <- c(10:20)
 
 # Create a vector `y` that has the values 21 through 30 in it (using the seq() function)
 
-
+y <- seq(21,30)
 # Create a vector `z` by combining the vectors `x` and `y`
+
+z <- c(x,y)
 
 
 # Create a variable `w` that contains the 11th element in your vector `z`
 
+w <- z[11]
+
 
 # Create a vector `q` that contains the second through the 5 elements of `z`
 
-
+q <- c(z[2:5]) #You can use a colon to choose the range of elements that you want in your new vector
 # Create a variable `len` that is equal to the length of your vector `q`
 
-
+len <- length(q)
 ### Bonus ###
 
 # Create a vector `even` that holds the even numbers from 1 to 100
 
-
+even <- seq(2,100,2)
 # Using the `all()` function and `%%` (modulo) operator, confirm that all of the
 # numbers in your `even` vector are even
+
+all(even %% 2 == 0)
+# The all function checks every element in the vector for the given conditions
+# modulo %% gives the remainder of the element divided by the given number (in this case, 2)
--- exercise-3/exercise.R
+++ exercise-3/exercise.R
@@ -2,23 +2,38 @@
 
 # Create a vector `marbles` with 6 colors (representing marbles).
 
+marbles <- c("red", "orange", "yellow", "green", "blue", "purple")
 
 # Use the `sample()` function to randomly select a single marble
 
-
+answer <- sample(marbles,1)
 # Define a function called MarbleGame. This function should:
 # - Takes in a single argument `guess` that is a guess at what color marble will be drawn
 # - Randomly samples a marble from the vector
 # - Returns whether or not the person guessed accurately (preferrably a character string phrase)
+num.guesses <- 0
+winner <- FALSE
+while (winner != TRUE) {
+  num.guesses <- (num.guesses + 1)
+  player.guess <- readline(prompt = "Enter your guess (a marble color): ")
+  winner <- MarbleGame(player.guess)
+}
+paste("You win! You guessed the correct color in", num.guesses,"tries!")
 
+MarbleGame <- function(guess) {
+  if(guess == answer){
+      return(TRUE)
+    } else {
+      print("Nope, that's not right!")
+      return(FALSE)
+    }
+}
 
 # Play the marble game by guessing a color!
 
 
 # Bonus: Play the marble game until you win, keeping track of how many tries you take
 # Hint, you'll need to use a `while` control structure, which is not covered in the module
-
-
 
 ## Double bonus(answer not provided): play the game 1000X (until you win) and track the average number of tries
 # Is it what you expected based on the probability
